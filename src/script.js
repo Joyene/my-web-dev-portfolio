c0c69309e68fd21cd8873ad9661c62aa9287373c
joyene <joymenyene@gmail.com>
2023-09-28 02:46:35 +0100

    intro-page completed

---
+++ src/script.js
@@ -0,0 +1,18 @@
+/*function displayWork() {
+  let works = document.getElementById("works");
+
+  if (works.style.display === "none") {
+    works.style.display === "block";
+  } else {
+    works.style.display === "none";
+  }
+}
+*/
+function displayWork() {
+  var x = document.getElementById("works");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
